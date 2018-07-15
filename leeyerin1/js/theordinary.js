2ad25cb841fa9314baefb11c238e2786b24e911f
ye-r1 <ye_r1@daum.net>
2018-07-16 00:47:47 +0900

    180716 ui add

--- leeyerin1/js/theordinary.js
+++ leeyerin1/js/theordinary.js
@@ -153,4 +153,32 @@ $(document).ready(function() {
     if ($(this).parent().hasClass('fix-on')) return false;
     $(this).parent().removeClass('on');
   });
+
+  showContents();
+
+  function showContents() {
+    var scrollTop = 0;
+    var offsetTop = 0;  //document all
+    var startEffect = 0;  //event start
+    var endEffect = 0;
+
+    checkScroll();
+    $(window).on('scroll resize', function() {
+      checkScroll();
+    });
+    
+    function checkScroll() {
+      scrollTop = $(document).scrollTop();
+      $('.content.scroll').each(function() {  //each
+        offsetTop = $(this).offset().top;
+        startEffect = offsetTop - $(window).height();   //event start = offset - window height
+        endEffect = offsetTop + $(this).outerHeight();  //event end = document height + this outer height
+        if (scrollTop > startEffect && scrollTop < endEffect) {
+          $(this).addClass('on');
+        } else {
+          $(this).removeClass('on');
+        }
+      });
+    }
+  }
 });
